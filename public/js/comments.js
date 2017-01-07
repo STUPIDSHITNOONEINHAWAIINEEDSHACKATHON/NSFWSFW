edf92665b99ea186e8bbd77ea1b293e86ebe5210
Nick Lee <nicklee808@gmail.com>
2017-01-07 12:36:12 -1000

    FINALLY SETUP

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,1 +1,31 @@
-function 
+//jshint esversion:6
+
+var subName = document.getElementById('userInput');
+console.log();
+document.getElementById('submit').addEventListener('click', subLoader);
+
+function subLoader () {
+
+  let subreddit = subName.value;
+
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', commentGenerator);
+  oReq.open('GET', `https://www.reddit.com/r/${subreddit}.json`);
+  oReq.send();
+
+
+}
+
+function commentGenerator (){
+  let objParse = JSON.parse(this.responseText);
+  let arrayOfPosts = objParse.data.children;
+  
+  console.log(arrayOfPosts);
+
+  let response = document.getElementById('response');
+  response.innerHTML = 'COMMENT';
+}
+
+
+
+// module.exports = 'comments';
