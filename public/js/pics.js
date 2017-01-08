4af1131110b8ffc1edfc5ba1436dc2fa0f857b98
jonathonlaylo <jonathon.laylo@gmail.com>
2017-01-07 14:40:26 -1000

    created png extents for pictures

--- public/js/pics.js
+++ public/js/pics.js
@@ -12,22 +12,38 @@ function picListener(data) {
     var picsArray = [];
 
     for (var i = 0; i < myData.length; i++) {
-      // console.log(myData[i].data.url);
       if(myData[i].data.url.toString().includes('reddituploads.com')){
-        // console.log('reddituploads: ', myData[i].data.url);
         var redditUpLoadsURL = myData[i].data.url.split('amp;').join('');
         // console.log(redditUpLoadsURL);
         picsArray.push(redditUpLoadsURL);
       } else {
         var URL = myData[i].data.url;
-        // console.log(URL);
-        picsArray.push(URL);
+        checkLink(URL);
+        // picsArray.push(URL);
       }
     }
+
+    function checkLink(URL) {
+      let ok = true;
+      let addExt = true;
+      var blackList = ['imageflake.com', 'gfycat', 'thnk1994.com', 'youtube.com', 'youtu.be', 'docs.google.com', 'flickr', 'instagram', '500px', 'pinkbike', '/a/', '/gallery/', '/r/'];
+      var whiteList = ['reddituploads', '.png', '.jpg', '.jpeg', '.gif'];
+      whiteList.map(str => {
+        if((URL.indexOf(str) > -1)?(true):(false)) {
+          addExt = false;
+        }
+      });
+      blackList.map(str => {
+        if((URL.indexOf(str) > -1)?(true):(false)) {
+          ok = false;
+        }
+      });
+      if(addExt) { URL += '.png'; }
+      if(ok) { picsArray.push(URL); }
+    }
+
     console.log(picsArray);
-    console.log(picsArray.length);
   }
 }
-
 picListener();
 
